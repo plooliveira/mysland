0f97da8b87b40135e702e002e949ce19050c786f
Allan Paladino <allanrp@gmail.com>
2020-10-31 13:10:58 -0300

    basic reservation done

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,11 +1,19 @@
 class ReservationsController < ApplicationController
 
-  def new
-    # TO DO
-  end
+  # reservation.status:
+  # 0 = pending payment
+  # 1 = payment done, pending approval by host
+  # 2 = approved by host
 
   def create
     # TO DO
+    @reservation = Reservation.new(reservation_params)
+    @island = Island.find(params[:island_id])
+    @reservation.island = @island
+    @reservation.status = 0
+    @reservation.user = current_user
+    @reservation.save
+    # INCLUIR REDIRECT PARA PAYMENT
   end
 
   def index
@@ -14,6 +22,12 @@ class ReservationsController < ApplicationController
 
   def show
     # TO DO
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:check_in, :check_out)
   end
 
 end
