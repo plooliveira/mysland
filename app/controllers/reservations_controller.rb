de050e5f6b576588fe58431a5d518b541dcdc180
Allan Paladino <allanrp@gmail.com>
2020-10-31 12:06:39 -0300

    generated reservations ocntroller and basic routes

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,15 @@
+class ReservationsController < ApplicationController
+
+  def create
+    # TO DO
+  end
+
+  def index
+    # TO DO
+  end
+
+  def show
+    # TO DO
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: 'pages#home'
-  resources :islands
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/about', to: 'pages#about'
+
+  resources :islands do
+    resources :reservations, only: [:create]
+  end
+
+  get '/reservations', to: 'reservations#index'
+  get '/reservations/:id', to: 'reservations#show'
+
 end
