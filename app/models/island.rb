b4b113a04d6838e13831b08abd061751126948c7
Allan Paladino <allanrp@gmail.com>
2020-11-05 19:11:20 -0300

    pg-search implemented on gemfile and model

--- app/models/island.rb
+++ app/models/island.rb
@@ -1,4 +1,13 @@
 class Island < ApplicationRecord
   belongs_to :user
-  has_many :users, through: :reservations 
+  has_many :users, through: :reservations
+
+
+  include PgSearch::Model
+  pg_search_scope :search_do_allan,
+    against: [ :name, :location, :description ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
+
 end
